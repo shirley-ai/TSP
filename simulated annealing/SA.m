e1cfb23b3aa4ccd75bfbd57914eb1604bf8bd718
shirley-ai <52014180+shirley-ai@users.noreply.github.com>
2019-06-21 13:05:12 +0100

    Add files via upload

---
+++ simulated annealing/SA.m
@@ -0,0 +1,13 @@
+% SA Î±´úÂë
+% choose initial solution
+% update best solution
+% while the system is not frozen/the final temporature is not reached
+% set iteration counter r=0, the number of neighbors to visit=m
+% repeat until r=m
+% generate a random neighbor of current solution and calculate the cost
+% if the new solution is better than the old solution OR ramdom(0,1)<acceptance
+% update the best solution
+% r=r+1
+% end the repeat if iteration times are met
+% reduce the temporature 
+
